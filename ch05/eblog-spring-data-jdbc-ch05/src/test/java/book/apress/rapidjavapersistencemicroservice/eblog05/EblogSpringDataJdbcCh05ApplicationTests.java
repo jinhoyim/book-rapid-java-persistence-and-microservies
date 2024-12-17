8a4b837a42952a832b36fff1d1be39629106c4b5
Jinho Yim <jinhoyim@outlook.com>
2024-12-17 14:40:02 +0900

    Spring Data JDBC 프로젝트 추가

---
+++ ch05/eblog-spring-data-jdbc-ch05/src/test/java/book/apress/rapidjavapersistencemicroservice/eblog05/EblogSpringDataJdbcCh05ApplicationTests.java
@@ -0,0 +1,13 @@
+package book.apress.rapidjavapersistencemicroservice.eblog05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EblogSpringDataJdbcCh05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
