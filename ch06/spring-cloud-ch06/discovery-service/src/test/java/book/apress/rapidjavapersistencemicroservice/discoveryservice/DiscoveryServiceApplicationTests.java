518e9d47f9f422358df10e3e85880ec89fd519ff
Jinho Yim <jinhoyim@outlook.com>
2024-12-18 12:52:07 +0900

    discovery-service 추가

---
+++ ch06/spring-cloud-ch06/discovery-service/src/test/java/book/apress/rapidjavapersistencemicroservice/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package book.apress.rapidjavapersistencemicroservice.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
