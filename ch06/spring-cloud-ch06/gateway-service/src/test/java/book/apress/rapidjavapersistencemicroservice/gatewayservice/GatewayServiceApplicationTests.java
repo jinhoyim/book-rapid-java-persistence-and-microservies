8335a3b63d69396c0e45127c6ad73383b9d30988
Jinho Yim <jinhoyim@outlook.com>
2024-12-21 16:08:39 +0900

    Spring Cloud Gateway Reactive Server 추가

---
+++ ch06/spring-cloud-ch06/gateway-service/src/test/java/book/apress/rapidjavapersistencemicroservice/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package book.apress.rapidjavapersistencemicroservice.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
